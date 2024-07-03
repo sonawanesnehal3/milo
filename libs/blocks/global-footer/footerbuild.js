d46f5e1511472a33a8b6eec9f3166f3bb96dcc9c
Snehal Sonawane <sonawane@Snehals-MacBook-Pro.local>
2024-07-03 05:48:19 +0530

    seperating css changes

--- libs/blocks/global-footer/footerbuild.js
+++ libs/blocks/global-footer/footerbuild.js
@@ -1,4 +1,5 @@
 import GlobalFooter from "../global-footer/global-footer.js";
 import "../global-footer/global-footer.css";
+import "../global-footer/global-footer-build.css";
 
 export { GlobalFooter };
